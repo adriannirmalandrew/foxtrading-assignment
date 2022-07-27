0cbb467b798fa5a9af7246507307e629a7f290a3
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-07-27 15:01:22 +0530

    Implemented fetching login encryption key

--- trader/server/index.js
+++ trader/server/index.js
@@ -9,6 +9,7 @@ const mongodb = require('mongodb');
 
 //Load environment variables
 require('dotenv').config();
+const USER_ID = process.env.USER_ID;
 const APP_CODE = process.env.APP_CODE;
 const SECRET_KEY = process.env.SECRET_KEY;
 
@@ -18,8 +19,26 @@ let tradeServer = express();
 const baseUrl = 'https://a3.aliceblueonline.com/rest/AliceBlueAPIService/api/';
 
 //Login
-tradeServer.post('/login', (req, res) => {
-	//TODO: Get 
+tradeServer.post('/login', async (req, res) => {
+	//Header to get encKey
+	let encKeyRequestHeaders = new Headers();
+	encKeyRequestHeaders.append('Content-Type', 'application/json');
+	//Request to get encKey
+	let encKeyRequest = new Request(baseUrl + 'customer/getAPIEncpkey', {
+		method: 'POST',
+		headers: encKeyRequestHeaders,
+		body: JSON.stringify({"userId": USER_ID}),
+		redirect: 'follow',
+	});
+	//Get encryption key
+	let encKeyResponse = await fetch(encKeyRequest).then(encResp => {
+		return encResp.text();
+	});
+	console.log(encKeyResponse);
+	//SHA-256 (user ID + API key + encryption key)
+	//Get session ID
+	//Return session ID
+	res.json(encKeyResponse);
 });
 
 //Logout
@@ -30,8 +49,9 @@ tradeServer.post('/logout', (req, res) => {
 //Buy
 tradeServer.post('/buy', (req, res) => {
 	//TODO
-})
+});
 
-//Sell
-
-//Get???
+//Start app
+tradeServer.listen(3000, () => {
+	console.log('Started trading server');
+});
