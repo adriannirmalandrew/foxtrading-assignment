4e2e1bcb372fa9c2f2f030453c357783050b3ebb
adriannirmalandrew <kingspammernerd@protonmail.com>
2022-07-27 13:40:39 +0530

    Added env variables with trader backend's API keys

--- trader/server/index.js
+++ trader/server/index.js
@@ -7,5 +7,31 @@ The backend for the "trader" application:
 const express = require('express');
 const mongodb = require('mongodb');
 
+//Load environment variables
+require('dotenv').config();
+const APP_CODE = process.env.APP_CODE;
+const SECRET_KEY = process.env.SECRET_KEY;
+
 //Initialize app
 let tradeServer = express();
+//Base URL:
+const baseUrl = 'https://a3.aliceblueonline.com/rest/AliceBlueAPIService/api/';
+
+//Login
+tradeServer.post('/login', (req, res) => {
+	//TODO: Get 
+});
+
+//Logout
+tradeServer.post('/logout', (req, res) => {
+	//TODO
+});
+
+//Buy
+tradeServer.post('/buy', (req, res) => {
+	//TODO
+})
+
+//Sell
+
+//Get???
